fc5abf1d21a9c673885cd575c1bb0808bbc6505b
Duda Nogueira <dudanogueira@gmail.com>
2013-01-04 14:19:35 -0200

    Add a basic boostrap page for the home

--- microerp/frontend_base/views.py
+++ microerp/frontend_base/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import RequestContext, loader, Context
 
 def home(request):
-    return render_to_response('frontend/base.html', locals(), context_instance=RequestContext(request),)
+    return render_to_response('frontend/main-home.html', locals(), context_instance=RequestContext(request),)
     
