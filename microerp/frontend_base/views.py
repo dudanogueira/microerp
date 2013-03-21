60b2e53ed1874c816f4b4d9e9e6385bcb4f5ff65
Duda Nogueira <dudanogueira@gmail.com>
2013-03-20 22:43:34 -0300

    Melhora o login

--- microerp/frontend_base/views.py
+++ microerp/frontend_base/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import RequestContext, loader, Context
 
+from django.contrib.auth.forms import AuthenticationForm
+
 def home(request):
+    form = AuthenticationForm()
+    a = "aaaa"
     return render_to_response('frontend/main-home.html', locals(), context_instance=RequestContext(request),)
