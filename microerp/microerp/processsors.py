699e7e61412b38cf8dc307049cf97b4b8f0cd81d
Duda Nogueira <dudanogueira@gmail.com>
2014-04-02 11:11:55 -0300

    Adiciona opçao AMBIENTE_TESTES=False por padrao no settings para exibir indicador de ambiente de testes

--- microerp/microerp/processsors.py
+++ microerp/microerp/processsors.py
@@ -2,10 +2,12 @@
 from django.conf import settings
 def logo_empresa(request):
     logo_empresa = getattr(settings, 'IMG_SRC_LOGO_EMPRESA', None)
+    ambiente_testes = getattr(settings, 'AMBIENTE_TESTES', False)
     mostra_menu_principal_lateral = getattr(settings, 'MOSTRAR_MENU_PRINCIPAL_LATERAL', True)
     rh_usa_banco_de_horas = getattr(settings, 'RH_USA_BANCO_DE_HORAS', True)
     return {
         'logo_empresa': logo_empresa,
         'mostra_menu_principal_lateral': mostra_menu_principal_lateral,
         'rh_usa_banco_de_horas': rh_usa_banco_de_horas,
+        'ambiente_testes': ambiente_testes,
         }
