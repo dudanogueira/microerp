121efa59f6a308fa10dbdf973a5353df0a4df1ee
Eduardo Nogueira <eduardonogueira@unimedtv.coop.br>
2015-11-04 10:27:55 -0200

    Melhoria na Interface Angular de Busca de CLientes

--- microerp/comercial/static/comercial/controllers.js
+++ microerp/comercial/static/comercial/controllers.js
@@ -5,25 +5,31 @@ ComercialApp.controller('ComercialAppCtrl', function ($scope, $http) {
 // LISTA PRE E CLIENTES
 ComercialApp.controller('PreClientesCtrl', function ($scope, $http, $routeParams, $location) {
     $scope.app = "Comercial";
-    $scope.q = $routeParams.q;
-    $scope.BuscaPreClientes = function () {
-        $location.search('q', $scope.q);
-        // clientes
-        $http.get("/comercial/api/cliente/?q="+$scope.q).success(function (data) {
-            $scope.clientes = data;
-        }).error(function (data, status) {
-            $scope.message = "Aconteceu um problema: " + data;
-        });
-        // preclientes
-        $http.get("/comercial/api/precliente/?q="+$scope.q).success(function (data) {
-            $scope.preclientes = data;
-        }).error(function (data, status) {
-            $scope.message = "Aconteceu um problema: " + data;
-        });
-    };
-    if ($scope.q){
-        $scope.BuscaPreClientes()
-    }
+
+
+    $scope.$watch('q', function (val) {
+        var payload = {'q': val};
+        if(val != '' && val != undefined && val.length > 2){
+            $scope.carregando = true
+            $http.get("/comercial/api/cliente/?q="+$scope.q).success(function (data) {
+                $scope.clientes = data;
+                $scope.carregando = false;
+            }).error(function (data, status) {
+                $scope.message = "Aconteceu um problema: " + data;
+            });
+            // preclientes
+            $http.get("/comercial/api/precliente/?q="+$scope.q).success(function (data) {
+                $scope.preclientes = data;
+                $scope.carregando = false;
+            }).error(function (data, status) {
+                $scope.message = "Aconteceu um problema: " + data;
+            });
+        }else{
+            $scope.preclientes = [];
+            $scope.clientes = [];
+        }
+    });
+
 });
 
 // DETALHE PRE CLIENTE
